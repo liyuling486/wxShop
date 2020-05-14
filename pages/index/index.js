6f34c0a8241926c1c1701933805684eb9dc973dd
12345678 <liyuling_5960@163.com>
2020-05-14 16:49:27 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,34 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    nav: [{
+      name: '时令水果'
+    }, {
+      name: '新鲜蔬菜'
+    }, {
+      name: '海鲜水产'
+    }, {
+      name: '速食冷冻'
+    }, {
+      name: '肉禽蛋品'
+    }, {
+      name: '量贩特惠'
+    }, {
+      name: '基地鲜花'
+    }, {
+      name: '美妆百货'
+    }, {
+      name: '母婴保健'
+    }, {
+      name: '进口酒饮'
+    }]
+  },
+
+  onLoad: function() {
+
+  }
+
+})
